2825de46b333849408e18e063328815867c164f2
Tamara <60240971+Prussak@users.noreply.github.com>
2023-01-05 16:11:42 -0300

    Add files via upload
    Exercicio Palindromo da aula O que é Ruby da DIO

---
+++ ExercicioPalindromo.rb
@@ -0,0 +1,16 @@
+##Exercicio Ruby Palindromo
+
+
+puts "Digite uma palavra"
+
+palavra = gets
+
+
+    if palavra.reverse == palavra
+
+        puts "Esta Palavra é um Palindromo"
+    else 
+        puts "Esta Palavra não é um Palindromo"
+        
+    end
+   
