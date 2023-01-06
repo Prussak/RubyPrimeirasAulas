e65dd18c5f23962d32a8df0e5063c715c72058f4
Tamara <60240971+Prussak@users.noreply.github.com>
2023-01-06 18:43:29 -0300

    Update Anotacao_Aula.rb

--- Anotacao_Aula.rb
+++ Anotacao_Aula.rb
@@ -88,3 +88,103 @@ puts c
 	carro = Carro.new
 	carro.adiciona_marca("Ford")
 	puts carro.marca
+	
+##########################################
+#Atributos virtuais e Acessors
+#attr_accessor, attr_reader, attr_writer
+	class Carro
+
+		attr_accessor :marca, :modelo
+
+		def velocidade_maxima
+			250	
+		end
+
+	end		
+	
+	carro = Carro.new
+	carro.marca ="Ford"
+	carro.modelo ="Focus"
+	puts "Marca: " + carro.marca
+	puts "Modelo: " + carro.modelo
+
+#Atributos virtuais
+class Carro
+
+		attr_accessor :marca, :modelo
+
+		def velocidade_maxima
+			250	
+		end
+	
+		def descricao
+			"Marca: #{@marca} e Modelo: #{@modelo}"
+	end		
+end	
+	carro = Carro.new
+	carro.marca ="Ford"
+	carro.modelo ="Focus"
+	puts "Marca: " + carro.marca
+	puts "Modelo: " + carro.modelo
+	puts "Descrição: " + carro.descricao
+	
+#######################################
+#Método Construtor	
+
+	class Carro
+	
+		attr_acessor :marca, :modelo
+		
+		def initialize(modelo, marca)
+			@modelo = modelo
+			@marca = marca
+		end
+	end	
+
+	carro = Carro.new("Model S", "Tesla")
+	puts carro
+	
+#######################################
+#Herança de Classes
+#Basic Object -> Object -> Automóvel { Carro e Moto }
+#super chama o método acelera da classe pai	
+
+		class Automovel
+			def acelera
+				#Aciona injeção eletronica
+				#injeta combustivel e etc...
+				puts "Acelerando automóvel..."
+			end
+		end
+
+		class Carro < Automovel
+			def acelera
+				#Verifica freios e outras coisas
+				#Que o carro especificamente necessita
+				puts "Verificando equipamentos..."
+				super
+			end
+		end	
+			
+	carro = Carro.new 
+	carro.acelera 		
+
+###########################################
+#Métodos de Classe	
+#Método privado somente a classe poderá ter acesso ao método
+
+	Class Automovel
+		def self.tipo_cambio
+			puts "Manual"
+		end
+
+	def acelera
+		puts "Acelerando automovel..."
+	end
+
+	private
+		def verifica_combustivel
+			puts"Verificando combustivel"
+		end	
+	end
+	
